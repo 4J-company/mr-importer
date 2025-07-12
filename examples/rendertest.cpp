8de5c1686fda600192bff31862ea8c84fa9962a3
Michael Tsukanov <tsukanovWOW@ya.ru>
2025-07-13 01:25:25 +0300

    Implement Manager & Conan (#2)
    * Add Conan script
    
    * Reimplement mr::Manager
    
    * Improve Conan script
    
    * Decouple mr::import and mr::Manager
    
    * Separate mr::Manager into its own Conan package

--- examples/rendertest.cpp
+++ examples/rendertest.cpp
@@ -1,5 +1,10 @@
-#include <mr-importer/importer.hpp>
+#include <filesystem>
+
+#include <mr-manager/manager.hpp>
+
 #include "render_polyscope.hpp"
+
+#include "mr-importer/importer.hpp"
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,7 +12,6 @@ int main(int argc, char **argv) {
     exit(47);
   }
 
-  auto asset = mr::import(argv[1]);
-
-  render(asset.meshes);
+  auto handle = mr::Manager<mr::Asset>::get().create("id", std::filesystem::path(argv[1]));
+  render(handle->meshes);
 }
---
+++ include/mr-importer/assets.cpp
@@ -0,0 +1,10 @@
+#include "importer.hpp"
+#include "assets.hpp"
+
+namespace mr {
+  inline namespace importer {
+    Asset::Asset(const std::filesystem::path &path) {
+      *this = import(path);
+    }
+  }
+}
